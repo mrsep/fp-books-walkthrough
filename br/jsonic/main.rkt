7b376eefb6d63001ff62515de298be7998fcb329
Hans Peschke <Hans.Peschke@ygomi.com>
2017-10-06 22:03:11 +0800

    add br:jsonic syntax coloring indentation buttons
    Signed-off-by: Hans Peschke <Hans.Peschke@ygomi.com>

--- br/jsonic/main.rkt
+++ br/jsonic/main.rkt
@@ -1,4 +1,19 @@
 #lang br/quicklang
 (module reader br
   (require "reader.rkt")
-  (provide read-syntax))
+  (provide read-syntax)
+  (provide get-info)
+
+  ; the function which is called by the ide (drracket)
+  (define (get-info port src-mod src-line src-col src-pos)
+    (define (handle-query key default)
+      (case key
+        ; dynamic-require imports functions at runtime if neede
+        [(color-lexer)
+         (dynamic-require 'jsonic/colorer  'color-jsonic)]
+        [(drracket:indentation)
+         (dynamic-require 'jsonic/indenter 'indent-jsonic)]
+        [(drracket:toolbar-buttons)
+         (dynamic-require 'jsonic/buttons  'button-list)]
+        [else default]))
+    handle-query))
