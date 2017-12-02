aa914dd47046624fa72fd538028e8876f011dc69
Hans Peschke <sep@guug.de>
2017-12-02 23:55:01 +0800

    finish aoc-2017-day2
    Signed-off-by: Hans Peschke <sep@guug.de>

--- aoc/2017/aoc-2017-clj/project.clj
+++ aoc/2017/aoc-2017-clj/project.clj
@@ -1,7 +1,8 @@
 (defproject aoc-2017-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Solutions for 2017th Advent of Code"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.4"]]
   :main aoc-2017-clj.core)
