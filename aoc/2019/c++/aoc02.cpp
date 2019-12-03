e55981dc23ea5361e6c16cea4ec8ed2285e5e36c
Hans Peschke <Hans.Peschke@ygomi.com>
2019-12-03 10:25:30 +0800

    aoc 2019-2 c++ finish second part
    Signed-off-by: Hans Peschke <Hans.Peschke@ygomi.com>

--- aoc/2019/c++/aoc02.cpp
+++ aoc/2019/c++/aoc02.cpp
@@ -8,10 +8,12 @@
 #include <fstream>
 #include <iomanip>
 
-using Int = long;
+using Int  = long;
+using Disk = std::vector<Int>;
+using Mem  = std::map<Int,Int>;
 
-std::vector<Int> readData(const std::string filename) {
-  std::vector<Int> result;
+Disk readData(const std::string filename) {
+  Disk result;
 
   std::ifstream ifs{filename};
   if (ifs) {
@@ -24,13 +26,13 @@ std::vector<Int> readData(const std::string filename) {
   return result;
 }
 
-std::map<Int,Int> convert(const std::vector<Int>& input) {
-  std::map<Int,Int> result;
+Mem convert(const Disk& input) {
+  Mem result;
   for (int i=0; i < input.size(); i++) { result[i] = input[i]; }
   return result;
 }
 
-bool execute(std::map<Int,Int>& mem, Int pc_) {
+bool execute(Mem& mem, Int pc_) {
   for (Int pc = pc_; ; pc += 4) {
     const Int opcode = mem[pc+0];
     const Int in1    = mem[mem[pc+1]];
@@ -47,20 +49,44 @@ bool execute(std::map<Int,Int>& mem, Int pc_) {
   }
 }
 
-int main() {
-  const auto input = readData("../aoc02.txt");
-  //for (const auto i : input) { std::cout << i << std::endl; }
-  auto mem = convert(input);
+Int answer1(const Disk& disk, Int noun, Int verb) {
+  Mem mem = convert(disk);
 
-  mem[1] = 12;
-  mem[2] =  2;
+  mem[1] = noun;
+  mem[2] = verb;
 
   if (!execute(mem, Int{})) {
     std::cerr << "something went wrong!" << std::endl;
   }
-  std::cout << "Answer 1: " << mem[0] << std::endl;
-  //std::cout << "Answer 2: " << 100*mem[1] + mem[2];
 
+  return mem[0];
+}
+
+Int answer2(const Disk& disk, Int target) {
+  for (Int noun = 0; noun < 1000; noun++) {
+    for (Int verb = 0; verb < 1000; verb++) {
+      if (answer1(disk, noun, verb) == target) {
+        return 100*noun+verb;
+      }
+    }
+  }
+
+  std::cerr << "Answer not found!" << std::endl;
+
+  return -1;
+}
+
+int main() {
+  const Disk disk = readData("../aoc02.txt");
+  //for (const auto i : disk) { std::cout << i << std::endl; }
+
+  std::cout << "Answer 1: " << answer1(disk, 12, 2) << std::endl;
+  std::cout << "Answer 2: " << answer2(disk, 19690720) << std::endl;
+
+  return 0;
+}
+
+void test() {
   const std::vector<Int> test1{ 1,9,10,3, 2,3,11,0, 99, 30,40,50 };
 
   auto mem1 = convert(test1);
