a74b0698a3924669a7e07ff2464d29c545c19d51
Hans Peschke <Hans.Peschke@mrsep.de>
2022-04-10 08:40:04 +0800

    rename 2015 prolog files to aocXX scheme and port to scryer prolog
    Signed-off-by: Hans Peschke <Hans.Peschke@mrsep.de>

--- aoc/2015/prolog/aoc01.pl
+++ aoc/2015/prolog/aoc01.pl
@@ -1,5 +1,7 @@
-:- use_module(library(clpfd)).
-:- use_module(library(apply)).
+:- use_module(library(dif)).
+:- use_module(library(clpz)).
+:- use_module(library(lists)).
+:- use_module(library(dcgs)).
 :- use_module(library(pio)).
 
 sum(   [],0).
@@ -12,14 +14,21 @@ cum_sum_helper([H|T], Acc, [AccN|TS]) :-
 cum_sum(   [],[]).
 cum_sum([H|T], [H|L]) :- cum_sum_helper(T, H, L).
 
-seq([])     --> [].
-seq([E|Es]) --> [E], seq(Es).
+% reimplemented due to bug in scryer prolog nth1
+nth0(0, [ E| _], E).
+nth0(I, [E0|Es], E) :-
+    dif(E0, E),
+    I1 #= I-1,
+    nth0(I1,Es,E).
+
+nth1(I, Es, E) :-
+    I0 #= I-1,
+    nth0(I0, Es, E).
 
 c2i('(',1).
 c2i(')',-1).
 
-sol01(L) :- phrase_from_file(seq(Codes), '../day01.txt'),
-            string_codes(Str,Codes), string_chars(Str,Chars),
+sol01(L) :- phrase_from_file(seq(Chars), '../day01.txt'),
             maplist(c2i,Chars,L).
 
 sol01a(X) :- sol01(L), sum(L,X).
